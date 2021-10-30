9e209f0babc31f0a393a37929cd840f72a497a35
Ognjen <opejovic@gmail.com>
2021-10-30 02:41:01 +0200

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import AppHome from "../views/AppHome.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: AppHome,
   },
   {
     path: "/about",
@@ -14,7 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/AppAbout.vue"),
   },
 ];
 
---
+++ src/views/AppHome.vue
@@ -0,0 +1,424 @@
+<template>
+  <div id="home" class="layout__grid" data-animate-in="fade">
+    <section class="home__left">
+      <p class="home__copy">
+        Helping people build quality web software, using cutting edge
+        technologies.
+      </p>
+
+      <router-link to="/about" class="home__cta"
+        >Read more about me</router-link
+      >
+    </section>
+
+    <section class="home__right">
+      <div class="home__blob">
+        <svg
+          id="home-svg"
+          width="496"
+          height="604"
+          viewBox="0 0 496 604"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            data-animate-in="fade"
+            opacity="0.0416667"
+            cx="261.184"
+            cy="254.093"
+            r="174.537"
+            transform="rotate(-115 261.184 254.093)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.0833333"
+            cx="274.708"
+            cy="263.557"
+            r="170.443"
+            transform="rotate(-110 274.708 263.557)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.125"
+            cx="286.536"
+            cy="274.273"
+            r="166.349"
+            transform="rotate(-105 286.536 274.273)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.166667"
+            cx="296.587"
+            cy="286.048"
+            r="162.255"
+            transform="rotate(-100 296.587 286.048)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.208333"
+            cx="304.8"
+            cy="298.689"
+            r="158.161"
+            transform="rotate(-95 304.8 298.689)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.25"
+            cx="311.135"
+            cy="311.996"
+            r="154.067"
+            transform="rotate(-90 311.135 311.996)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.291667"
+            cx="315.572"
+            cy="325.774"
+            r="149.973"
+            transform="rotate(-85 315.572 325.774)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.333333"
+            cx="318.113"
+            cy="339.827"
+            r="145.88"
+            transform="rotate(-80 318.113 339.827)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.375"
+            cx="318.774"
+            cy="353.964"
+            r="141.786"
+            transform="rotate(-75 318.774 353.964)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.416667"
+            cx="317.596"
+            cy="367.997"
+            r="137.692"
+            transform="rotate(-70 317.596 367.997)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.458333"
+            cx="314.634"
+            cy="381.749"
+            r="133.598"
+            transform="rotate(-65 314.634 381.749)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.5"
+            cx="309.96"
+            cy="395.049"
+            r="129.504"
+            transform="rotate(-60 309.96 395.049)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.541667"
+            cx="303.662"
+            cy="407.74"
+            r="125.41"
+            transform="rotate(-55 303.662 407.74)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.583333"
+            cx="295.844"
+            cy="419.674"
+            r="121.316"
+            transform="rotate(-50 295.844 419.674)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.625"
+            cx="286.622"
+            cy="430.718"
+            r="117.222"
+            transform="rotate(-45 286.622 430.718)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.666667"
+            cx="276.122"
+            cy="440.753"
+            r="113.129"
+            transform="rotate(-40 276.122 440.753)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.708333"
+            cx="264.48"
+            cy="449.675"
+            r="109.035"
+            transform="rotate(-35 264.48 449.675)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.75"
+            cx="251.842"
+            cy="457.397"
+            r="104.941"
+            transform="rotate(-30 251.842 457.397)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.791667"
+            cx="238.359"
+            cy="463.847"
+            r="100.847"
+            transform="rotate(-25 238.359 463.847)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.833333"
+            cx="224.186"
+            cy="468.972"
+            r="96.7531"
+            transform="rotate(-20 224.186 468.972)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.875"
+            cx="209.481"
+            cy="472.736"
+            r="92.6592"
+            transform="rotate(-15 209.481 472.736)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.916667"
+            cx="194.402"
+            cy="475.119"
+            r="88.5653"
+            transform="rotate(-10 194.402 475.119)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            opacity="0.958333"
+            cx="179.109"
+            cy="476.119"
+            r="84.4714"
+            transform="rotate(-5 179.109 476.119)"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+          <circle
+            data-animate-in="fade"
+            cx="163.755"
+            cy="475.751"
+            r="80.3775"
+            stroke="#9A66FF"
+            stroke-width="3"
+          />
+        </svg>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {},
+
+  mounted() {
+    const svg = document.getElementById("home-svg");
+    const children = svg?.children;
+
+    setTimeout(() => {
+      const home = document.getElementById("home");
+      home.classList.add("in-view");
+    });
+
+    // let length = children?.length;
+
+    setTimeout(() => {
+      children[0].classList.add("in-view-svgs");
+      children[0].style.opacity = children[0].attributes.opacity.nodeValue;
+    }, 500);
+
+    setTimeout(() => {
+      children[1].classList.add("in-view-svgs");
+      children[1].style.opacity = children[1].attributes.opacity.nodeValue;
+    }, 600);
+
+    setTimeout(() => {
+      children[2].classList.add("in-view-svgs");
+      children[2].style.opacity = children[2].attributes.opacity.nodeValue;
+    }, 700);
+
+    setTimeout(() => {
+      children[3].classList.add("in-view-svgs");
+      children[3].style.opacity = children[3].attributes.opacity.nodeValue;
+    }, 800);
+
+    setTimeout(() => {
+      children[4].classList.add("in-view-svgs");
+      children[4].style.opacity = children[4].attributes.opacity.nodeValue;
+    }, 900);
+
+    setTimeout(() => {
+      children[5].classList.add("in-view-svgs");
+      children[5].style.opacity = children[5].attributes.opacity.nodeValue;
+    }, 1000);
+
+    setTimeout(() => {
+      children[6].classList.add("in-view-svgs");
+      children[6].style.opacity = children[6].attributes.opacity.nodeValue;
+    }, 1100);
+
+    setTimeout(() => {
+      children[7].classList.add("in-view-svgs");
+      children[7].style.opacity = children[7].attributes.opacity.nodeValue;
+    }, 1200);
+
+    setTimeout(() => {
+      children[8].classList.add("in-view-svgs");
+      children[8].style.opacity = children[8].attributes.opacity.nodeValue;
+    }, 1300);
+
+    setTimeout(() => {
+      children[9].classList.add("in-view-svgs");
+      children[9].style.opacity = children[9].attributes.opacity.nodeValue;
+    }, 1400);
+
+    setTimeout(() => {
+      children[10].classList.add("in-view-svgs");
+      children[10].style.opacity = children[10].attributes.opacity.nodeValue;
+    }, 1500);
+    setTimeout(() => {
+      children[11].classList.add("in-view-svgs");
+      children[11].style.opacity = children[11].attributes.opacity.nodeValue;
+    }, 1600);
+    setTimeout(() => {
+      children[12].classList.add("in-view-svgs");
+      children[12].style.opacity = children[12].attributes.opacity.nodeValue;
+    }, 1700);
+    setTimeout(() => {
+      children[13].classList.add("in-view-svgs");
+      children[13].style.opacity = children[13].attributes.opacity.nodeValue;
+    }, 1800);
+    setTimeout(() => {
+      children[14].classList.add("in-view-svgs");
+      children[14].style.opacity = children[14].attributes.opacity.nodeValue;
+    }, 1900);
+    setTimeout(() => {
+      children[15].classList.add("in-view-svgs");
+      children[15].style.opacity = children[15].attributes.opacity.nodeValue;
+    }, 2000);
+    setTimeout(() => {
+      children[16].classList.add("in-view-svgs");
+      children[16].style.opacity = children[16].attributes.opacity.nodeValue;
+    }, 2100);
+    setTimeout(() => {
+      children[17].classList.add("in-view-svgs");
+      children[17].style.opacity = children[17].attributes.opacity.nodeValue;
+    }, 2200);
+    setTimeout(() => {
+      children[18].classList.add("in-view-svgs");
+      children[18].style.opacity = children[18].attributes.opacity.nodeValue;
+    }, 2300);
+    setTimeout(() => {
+      children[19].classList.add("in-view-svgs");
+      children[19].style.opacity = children[19].attributes.opacity.nodeValue;
+    }, 2400);
+    setTimeout(() => {
+      children[20].classList.add("in-view-svgs");
+      children[20].style.opacity = children[20].attributes.opacity.nodeValue;
+    }, 2500);
+    setTimeout(() => {
+      children[21].classList.add("in-view-svgs");
+      children[21].style.opacity = children[21].attributes.opacity.nodeValue;
+    }, 2600);
+    setTimeout(() => {
+      children[22].classList.add("in-view-svgs");
+      children[22].style.opacity = children[22].attributes.opacity.nodeValue;
+    }, 2700);
+    setTimeout(() => {
+      children[23].classList.add("in-view-svgs");
+      children[23].style.opacity = 1;
+    }, 2800);
+
+
+    // setInterval(() => {
+    //   children[23].style.transform = 'scale(2)';
+    //   // children[23].style.opacity = children[23].attributes.opacity.nodeValue;
+    // }, 5000);
+
+    // for (let circle of children) {
+    //   setTimeout(() => {
+    //     circle.
+    //   }, 300);
+    // }
+
+    // setTimeout(() => {
+    //   const text = document.getElementById('animateFirst')
+    //   text.classList.add('in-view')
+    //   const mobileText = document.getElementById('animateFirstMobile')
+    //   mobileText.classList.add('in-view')
+    // }, 300)
+
+    // setTimeout(() => {
+    //   const text = document.getElementById('animateSecond')
+    //   text.classList.add('in-view')
+    // }, 500)
+
+    // setTimeout(() => {
+    //   const sayHello = document.getElementById('animateThird')
+    //   sayHello.classList.add('in-view')
+    // }, 700)
+  },
+};
+</script>
